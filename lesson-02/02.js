0c982b7602a7d44e913ac947533bc057d2f14574
volktasher <supervladworld@gmail.com>
2025-04-15 16:34:49 +0300

    complete task 7

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -9,10 +9,10 @@
 
 let dice1 = Math.floor(Math.random() * 6) + 1;  // your code
 let dice2 = Math.floor(Math.random() * 6) + 1; // your code
-let isWinningDouble = (dice1 === dice2) && (dice1 > 3);
+let isWinningDouble = (dice1 === dice2) && (dice1 >= 3);
 // your code
 
-/*
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
@@ -20,4 +20,4 @@ if (isWinningDouble) {
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
+
